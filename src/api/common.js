c542bb557279e834d64fcc9bca3f733f9d5f3145
Luke2327 <32826739+luke2327@users.noreply.github.com>
2020-07-02 01:45:20 +0900

    Update common.js

--- src/api/common.js
+++ src/api/common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURIScheme = "http://ec2-13-125-229-136.ap-northeast-2.compute.amazonaws.com:3050/";
+const apiURIScheme = "http://localhost:3050/";
 
 export default {
   send: async (url) => {
